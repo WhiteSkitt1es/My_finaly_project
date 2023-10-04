afe6fcba28b1537dd36907069a6d67e6a3f160d7
Pavel Artem'yev <pasha.artemev48@gmail.com>
2023-10-04 15:42:14 +0300

    014 Компонент Footer

---
+++ client/src/layouts/NavigationsAndFooter/Footer.tsx
@@ -0,0 +1,24 @@
+function Footer () {
+    return (
+        <div className='main-color'>
+      <footer className='container d-flex flex-wrap 
+                justify-content-between align-items-center py-5 main-color'>
+        <p className='col-md-4 mb-0 text-white'>© books.pavelartemev.tech</p>
+        <ul className='nav navbar-dark col-md-4 justify-content-end'>
+          <li className='nav-item'>
+            <a href='/' className='nav-link px-2 text-white'>
+              Home
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/search' className='nav-link px-2 text-white'>
+                Book Search
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+    );
+}
+
+export default Footer;
