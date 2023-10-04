ed917aa54f5359c515e28c879d49cae253b07774
Pavel Artem'yev <pasha.artemev48@gmail.com>
2023-10-04 15:43:43 +0300

    015 Внедрение Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavigationsBar from './layouts/NavigationsAndFooter/NavigationsBar';
 import HomePage from './layouts/HomePage/HomePage';
+import Footer from './layouts/NavigationsAndFooter/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavigationsBar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
